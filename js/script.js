68fd911a506f80b556cc0c0acefb3f2a9651ee58
Marko Cizmar <m.cizmar@emakina.rs>
2021-11-22 16:58:36 +0100

    Added final page
    nije gotovo, ali imam dosta modified fajlova pa ga pushujem cisto za svaki slucaj

--- js/script.js
+++ js/script.js
@@ -71,3 +71,79 @@ function openAccordion() {
   this.nextElementSibling.classList.add("show");
   this.classList.add("active");
 }
+
+//------------CHECKOUT STEPS------------//
+
+const btn = document.querySelectorAll(".checkout-step");
+const view = document.querySelectorAll(".checkout-view");
+const nextStep = document.querySelector(".next-step");
+
+function hideView(array) {
+  Array.from(array).forEach((el) => {
+    el.classList.remove("show");
+  });
+}
+
+function showView(id, e) {
+  hideView(btn);
+  hideView(view);
+  document.getElementById(id).classList.add("show");
+}
+
+Array.from(btn).forEach((el) => {
+  el.addEventListener("click", function (e) {
+    const id = this.getAttribute("data-toggle");
+    const activTab = document.querySelector(".checkout-step.active");
+    const activId = activTab.getAttribute("data-toggle");
+    if (Number(id) < Number(activId) || this.classList.contains("active")) {
+      showView(id);
+      this.classList.add("active");
+      this.classList.add("show");
+    }
+  });
+});
+
+//------------DONATION BTN----------//
+
+const donbtn = document.querySelectorAll(".donation-btn");
+
+donbtn.forEach((n) => n.addEventListener("click", changeColor));
+
+function changeColor() {
+  donbtn.forEach((el) => el.classList.remove("active"));
+  this.classList.add("active");
+}
+
+//--------------DONATIONS DROPDOWN------------//
+
+var isOpen = false;
+
+function toggleOptions(e) {
+  isOpen = !isOpen;
+  if (isOpen) {
+    document.getElementById("selectContainer").classList.add("active");
+  } else {
+    document.getElementById("selectContainer").classList.remove("active");
+  }
+}
+
+function selected(val) {
+  document.getElementById("valueText").innerHTML = val;
+  document.getElementById("selectedValue").val = val;
+  toggleOptions();
+}
+
+const chview = document.querySelectorAll(".checkout-view");
+
+chview.forEach((el) => {
+  el.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const id = el.getAttribute("id");
+    const link = document.querySelector(`.checkout-step[data-toggle="${id}"]`);
+    link.setAttribute("data-completed", 1);
+    link.nextElementSibling.classList.add("active");
+    link.classList.remove("active");
+    const newId = link.nextElementSibling.getAttribute("data-toggle");
+    showView(newId);
+  });
+});
